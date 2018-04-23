b8e1820ee690fb092872f7751af88bc4ff9872fa
Justin Roller <justin@>
2018-04-23 16:44:34 -0700

    add more code for testing description and item list.

--- ToDoList.Tests/ModelTests/ItemTest.cs
+++ ToDoList.Tests/ModelTests/ItemTest.cs
@@ -1,11 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoListApp.Models;
+using ToDoListApp;
+using System;
+using System.Collections.Generic;
 
-namespace ToDoListApp.Controllers
+namespace ToDoListApp.Tests
 {
   [TestClass]
-  public class ItemTest
+  public class ItemTest : IDisposable
   {
+      public void Dispose()
+    {
+      Item.ClearAll();
+    }
     [TestMethod]
     public void GetDescription_ReturnsDescription_String()
     {
@@ -19,6 +26,29 @@ namespace ToDoListApp.Controllers
 
       //Assert
       Assert.AreEqual(description, result);
+    
+    }
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      //Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      newItem1.Save();
+      Item newItem2 = new Item(description02);
+      newItem2.Save();
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
+
+      //Act
+      List<Item> result = Item.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+      foreach (Item thisItem in result)
+      {
+        Console.WriteLine("Output: " + thisItem.GetDescription());
+      }
     }
   }
 }
